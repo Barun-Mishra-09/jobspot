0b46c949e0eca6806e6b9250d3ddbf5de28b193b
Barun Kumar Mishra <144221171+Barun-Mishra-09@users.noreply.github.com>
2026-08-19 13:12:20 +0530

    Fix CORS origin URL for production

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ const corsOptions = {
   origin:
     process.env.CLIENT_URL ||
     "http://localhost:5173" ||
-    "https://jobspot-om79.onrender.com/login",
+    "https:https://jobspot-jobsearch.onrender.com/login",
   credentials: true,
 };
 app.use(cors(corsOptions));
